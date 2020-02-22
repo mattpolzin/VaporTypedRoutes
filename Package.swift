b0b64163b6a115a196ede506c4779445bf81c084
Mathew Polzin <matt.polzin@gmail.com>
2020-02-21 16:26:52 -0800

    switch to tools version 5.2, update vapor version.

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.2
 
 import PackageDescription
 
@@ -13,14 +13,18 @@ let package = Package(
             targets: ["VaporTypedRoutes"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0-alpha.3"),
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0-beta.3.24"),
     ],
     targets: [
         .target(
             name: "VaporTypedRoutes",
-            dependencies: ["Vapor"]),
+            dependencies: [.product(name: "Vapor", package: "vapor")]),
         .testTarget(
             name: "VaporTypedRoutesTests",
-            dependencies: ["VaporTypedRoutes", "Vapor", "XCTVapor"]),
+            dependencies: [
+                "VaporTypedRoutes",
+                .product(name: "Vapor", package: "vapor"),
+                .product(name: "XCTVapor", package: "vapor")
+        ]),
     ]
 )
