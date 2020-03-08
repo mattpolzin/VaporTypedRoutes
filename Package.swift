a7b1fae2537fd516ab083fa92f2d6a94d02cb46c
Mathew Polzin <matt.polzin@gmail.com>
2020-03-07 17:52:47 -0800

    update to latest vapor rc

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "VaporTypedRoutes",
     platforms: [
-        .macOS(.v10_14)
+        .macOS(.v10_15)
     ],
     products: [
         .library(
@@ -13,7 +13,7 @@ let package = Package(
             targets: ["VaporTypedRoutes"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0-beta.3.24"),
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0-rc.3"),
     ],
     targets: [
         .target(
