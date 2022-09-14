819159f80aac13f46cbf7f49ed22f87b129ef1c4
Charlie Welsh <charlie@allotrope.dev>
2022-09-13 23:55:25 -0700

    Add documentation

--- Sources/VaporTypedRoutes/ResponseBuilder.swift
+++ Sources/VaporTypedRoutes/ResponseBuilder.swift
@@ -9,25 +9,42 @@ import Vapor
 
 @dynamicMemberLookup
 public struct ResponseBuilder<Context: RouteContext> {
+	/// The context for the route.
     public let context: Context = .shared
+	/// The associated request for the route.
     private unowned var request: TypedRequest<Context>
 
+	/// Takes a KeyPath to a response context and returns a `ResponseEncoder` using the given `ResponseContext`.
+	///
+	/// - Parameters:
+	///   - dynamicMember: A KeyPath from the route's `Context` to a  `ResponseContext` to use in the `ResponseEncoder`.
     public subscript<T>(dynamicMember path: KeyPath<Context, ResponseContext<T>>) -> ResponseEncoder<T> {
         return .init(request: request, modifiers: [context[keyPath: path].configure])
     }
 
+	/// Takes a KeyPath to a canned response and returns an `EventLoopFuture` with the canned response.
+	///
+	/// - Parameters:
+	///   - dynamicMember: A KeyPath from the route's `Context` to a  `CannedResponse` to return in the `EventLoopFuture`.
     public subscript<T>(dynamicMember path: KeyPath<Context, CannedResponse<T>>) -> EventLoopFuture<Response> {
         return request.eventLoop.makeSucceededFuture(context[keyPath: path].response)
     }
 
+	/// Create a response builder with a given `TypedRequest`.
     public init(request: TypedRequest<Context>) {
         self.request = request
     }
 
+	/// A response encoder for a given body type.
     public struct ResponseEncoder<ResponseBodyType: ResponseEncodable> {
+		/// The associated request.
         private let request: TypedRequest<Context>
+		/// A set of functions used to modify the request.
         private let modifiers: [(inout Response) -> Void]
 
+		/// Returns an `EventLoopFuture` containing the encoded response, using a given instance of `ResponseBodyType`.
+		/// - Parameters:
+		///   - response: The instance of `ResponseBodyType` to use to generate the response.
         public func encode(_ response: ResponseBodyType) -> EventLoopFuture<Response> {
             let encodedResponseFuture = response
                 .encodeResponse(for: request.underlyingRequest)
@@ -38,6 +55,10 @@ public struct ResponseBuilder<Context: RouteContext> {
             }
         }
 
+		/// Create a `ResponseEncoder` using a given `TypedRequest` and set of modifiers.
+		/// - Parameters:
+		///   - request: The `TypedRequest` to use to generate the `Response`.
+		///   - modifiers: The functions to use to modify thr request.
         init(request: TypedRequest<Context>, modifiers: [(inout Response) -> Void]) {
             self.request = request
             self.modifiers = modifiers
--- Sources/VaporTypedRoutes/ResponseContext.swift
+++ Sources/VaporTypedRoutes/ResponseContext.swift
@@ -7,22 +7,34 @@
 
 import Vapor
 
+/// A type that represents an abstract response context.
+///
+/// The configured response is what gets sent to the user.
 public protocol AbstractResponseContextType {
+	/// A function used to configure the response.
+	/// - Parameters:
+	///   - response: The response to configure.
     var configure: (inout Response) -> Void { get }
+	/// The type of the response's body.
     var responseBodyType: Any.Type { get }
 }
 
+/// A type that represents a concrete response context.
 public protocol ResponseContextType: AbstractResponseContextType {
+	/// The associated response body type.
     associatedtype ResponseBodyType: ResponseEncodable
 }
 
 extension ResponseContextType {
+	/// The type of the response body.
     public var responseBodyType: Any.Type { return ResponseBodyType.self }
 }
 
+/// A concrete response context with an associated body type.
 public struct ResponseContext<ResponseBodyType: ResponseEncodable>: ResponseContextType {
     public let configure: (inout Response) -> Void
 
+	/// Create a response context with a given configuration function.
     public init(_ configure: @escaping (inout Response) -> Void) {
         self.configure = { response in
             configure(&response)
@@ -30,10 +42,15 @@ public struct ResponseContext<ResponseBodyType: ResponseEncodable>: ResponseCont
     }
 }
 
+/// A response context that always sends a given pre-determined response.
 public struct CannedResponse<ResponseBodyType: ResponseEncodable>: ResponseContextType {
     public let configure: (inout Response) -> Void
+	/// The response to send to the user.
     public let response: Response
 
+	/// Create a canned response with a given response to send.
+	/// - Parameters:
+	///   - response: The response to send to the user.
     public init(response cannedResponse: Response) {
         self.response = cannedResponse
         self.configure = { response in
